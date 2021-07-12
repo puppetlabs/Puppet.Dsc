e0be0e17da66aca08f913f207e16950288b9678c
Michael T Lombardi <michael.t.lombardi@gmail.com>
2021-07-12 11:58:41 -0500

    (MAINT) Update util functions
    This commit updates the util functions based on needs from latest release
    cycle. Future commits will pull each of these functions into the Puppet.Dsc
    module and add documentation/unit tests.

--- extras/util.ps1
+++ extras/util.ps1
@@ -1,3 +1,24 @@
+$ProjectRoot = Split-Path -Parent $PSCommandPath |
+  Split-Path -Parent |
+  Resolve-Path
+
+$ModulePath = Join-Path -Path $ProjectRoot -ChildPath 'src\puppet.dsc.psd1' | Resolve-Path
+Import-Module $ModulePath -Force
+
+$PrivateFunctionsFolder = Join-Path -Path $ProjectRoot -ChildPath 'src\internal\functions' | Resolve-Path
+$PrivateFunctionsToLoad = @(
+  'Invoke-PdkCommand'
+  'Out-Utf8File'
+  'ConvertTo-UnescapedJson'
+)
+Get-ChildItem -Path $PrivateFunctionsFolder |
+  Where-Object -FilterScript {
+    $_.BaseName -in $PrivateFunctionsToLoad
+  } |
+  ForEach-Object -Process {
+    . $_.FullName
+  }
+
 Function ConvertTo-VersionBuild {
   [CmdletBinding()]
   param (
@@ -62,7 +83,16 @@ Function Get-ForgeModuleInfo {
   param (
     [Parameter()]
     [string[]]
-    $Name
+    $Name,
+    [Parameter()]
+    [datetime]
+    $Until,
+    [Parameter()]
+    [datetime]
+    $Since,
+    [Parameter()]
+    [string[]]
+    $SkipVersion
   )
 
   Begin {
@@ -79,6 +109,25 @@ Function Get-ForgeModuleInfo {
     foreach ($Module in $Name) {
       $ModuleSearchParameters.Uri = $UriBase + (Get-PuppetizedModuleName $Module)
       $Result = Invoke-RestMethod @ModuleSearchParameters
+      # Filter Releases:
+      if ($null -ne $Until) {
+        $Result.releases = $Result.releases | Where-Object -FilterScript {
+          [datetime]$_.created_at -lt $Until
+        }
+      }
+      if ($null -ne $Since) {
+        $Result.releases = $Result.releases | Where-Object -FilterScript {
+          [datetime]$_.created_at -gt $Since
+        }
+      }
+      If ($null -ne $SkipVersion) {
+        $Result.releases = $Result.releases | Where-Object -FilterScript {
+          $null = $_.version -match '(?<Version>\d+\.\d+\.\d+-\d+)-(?<Build>\d+$)'
+          $ReleaseVersion = $Matches.Version
+          $ReleaseVersion -notin $SkipVersion
+        }
+      }
+
       [PSCustomObject]@{
         Name                 = $Result.name
         Releases             = $Result.releases.version
@@ -93,8 +142,11 @@ Function Get-ForgeDscModules {
   [CmdletBinding()]
   param (
     [Parameter()]
-    [object[]]
-    $Name
+    [datetime]
+    $Until,
+    [Parameter()]
+    [datetime]
+    $Since
   )
 
   Begin {
@@ -119,6 +171,17 @@ Function Get-ForgeDscModules {
     do {
       $Response = Invoke-RestMethod @ForgeSearchParameters
       ForEach ($Result in $Response.results) {
+        # Filter Releases:
+        if ($null -ne $Until) {
+          $Result.releases = $Result.releases | Where-Object -FilterScript {
+            [datetime]$_.created_at -lt $Until
+          }
+        }
+        if ($null -ne $Since) {
+          $Result.releases = $Result.releases | Where-Object -FilterScript {
+            [datetime]$_.created_at -gt $Since
+          }
+        }
         $null = $Results.Add([PSCustomObject]@{
             Name                 = $Result.name
             Releases             = $Result.releases.version
@@ -132,49 +195,132 @@ Function Get-ForgeDscModules {
   End { }
 }
 
-Function Update-ForgeDscModule {
+Function Export-PuppetModule {
   [CmdletBinding()]
-  param (
-    [Parameter()]
-    [string[]]$Name
+  Param (
+    [string]$FolderToExecuteIn,
+    [string]$ExportFolder
+  )
+
+  If ([string]::IsNullOrEmpty($ExportFolder)) {
+    $Command = 'pdk build'
+  } Else {
+    $Command = "pdk build --target-dir $ExportFolder"
+  }
+  Invoke-PdkCommand -Path $FolderToExecuteIn -Command $Command -SuccessFilterScript {
+    $_ -match 'has completed successfully. Built package can be found here'
+  }
+}
+
+Function Publish-PuppetDscModule {
+  [CmdletBinding()]
+  Param (
+    [string]$FolderToExecuteIn,
+    [string]$PackagedModulePath
+  )
+  If ([string]::IsNullOrEmpty($PackagedModulePath)) {
+    Write-Verbose "Publishing from $FolderToExecuteIn"
+    $Command = "pdk release publish --forge-token $env:FORGE_TOKEN"
+  } Else {
+    Write-Verbose "Publishing $PackagedModulePath"
+    $Command = "pdk release publish --forge-token $env:FORGE_TOKEN --file $PackagedModulePath"
+  }
+
+  Invoke-PdkCommand -Path $FolderToExecuteIn -Command $Command -SuccessFilterScript {
+    $_ -match 'Publish to Forge was successful'
+  }
+}
+
+Function Set-PuppetModuleVersion {
+  [CmdletBinding()]
+  Param (
+    [string]$FolderToExecuteIn,
+    [string]$Version
   )
 
   Begin { }
 
   Process {
+    $PuppetMetadataJsonPath = Join-Path -Path $FolderToExecuteIn -ChildPath 'metadata.json' | Resolve-Path
+    $PuppetMetadata = Get-Content -Path $PuppetMetadataJsonPath -Raw | ConvertFrom-Json
+    $PuppetMetadata.version = $Version
+    $PuppetMetadataJson = ConvertTo-UnescapedJson -InputObject $PuppetMetadata -Depth 10
+    Out-Utf8File -Path $PuppetMetadataJsonPath -InputObject $PuppetMetadataJson
+  }
+
+  End { }
+}
+Function Update-ForgeDscModule {
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [string[]]$Name,
+    [Parameter()]
+    [datetime]
+    $Until,
+    [Parameter()]
+    [datetime]
+    $Since,
+    [Parameter()]
+    [string[]]
+    $SkipVersion,
+    [string]$BuildFolderRoot,
+    [string]$PackageFolder
+  )
+
+  Begin { }
+
+  Process {
+    $FilterParams = @{}
+    If ($null -ne $Until) { $FilterParams.Until = $Until }
+    If ($null -ne $Since) { $FilterParams.Since = $Since }
     If ($null -eq $Name) {
-      $ModulesToRebuild = Get-ForgeDscModules
+      Write-Host "Looking for DSC modules on the forge with the filter:`r`n$($FilterParams | Out-String)"
+      $ModulesToRebuild = Get-ForgeDscModules @FilterParams
     } Else {
-      $ModulesToRebuild = Get-ForgeModuleInfo -Name $Name
+      Write-Host "Looking for the $Name DSC module on the forge with the filter:`r`n$($FilterParams | Out-String)"
+      If ($null -ne $SkipVersion) { $FilterParams.SkipVersion = $SkipVersion }
+      $ModulesToRebuild = Get-ForgeModuleInfo -Name $Name @FilterParams
     }
     foreach ($Module in $ModulesToRebuild) {
       foreach ($VersionAndBuild in (Get-LatestBuild $Module.Releases)) {
-        $OutputFolder = "$(Get-Location)/import/$($Module.Name)"
-        If (Test-Path $OutputFolder) {
-          Remove-Item $OutputFolder -Force -Recurse
+        try {
+          $ErrorActionPreference = 'Stop'
+          $BuildFolder = Join-Path -Path $BuildFolderRoot -ChildPath $Module.Name
+          If (Test-Path $BuildFolder) {
+            Remove-Item $BuildFolder -Force -Recurse
+          }
+          $PuppetizeParameters = @{
+            PuppetModuleAuthor      = 'dsc'
+            PowerShellModuleName    = [string]$Module.PowerShellModuleInfo.Name
+            PowerShellModuleVersion = [string]$VersionAndBuild.Version -replace '-', '.'
+          }
+          If (![string]::IsNullOrEmpty($BuildFolderRoot)) { $PuppetizeParameters.OutputDirectory = $BuildFolderRoot }
+          Write-Host "Puppetizing with:`r`n$($PuppetizeParameters | Out-String)"
+          New-PuppetDscModule @PuppetizeParameters
+          $NewVersion = $VersionAndBuild | ConvertTo-Json | ConvertFrom-Json
+
+          $Newversion.Build += 1
+          $NewVersion = $NewVersion | ConvertFrom-VersionBuild
+          Write-Host "Bumping build version from $($VersionAndBuild | ConvertFrom-VersionBuild) to $NewVersion"
+          Set-PuppetModuleVersion -FolderToExecuteIn $BuildFolder -Version $NewVersion
+          If ([string]::IsNullOrEmpty($PackageFolder)) {
+            Export-PuppetModule -FolderToExecuteIn $BuildFolder
+            Write-Host "Publishing $($Module.Name) at $NewVersion"
+            Publish-PuppetDscModule -FolderToExecuteIn $BuildFolder
+          } Else {
+            Export-PuppetModule -FolderToExecuteIn $BuildFolder -ExportFolder $PackageFolder
+            $PackagedModulePath = Get-ChildItem -Path $PackageFolder | Where-Object -FilterScript {
+              $_.BaseName -match [regex]::escape($Module.Name) -and $_.BaseName -match $NewVersion
+            } | Select-Object -ExpandProperty FullName
+            Write-Host "Publishing $($Module.Name) from $PackagedModulePath"
+            Publish-PuppetDscModule -FolderToExecuteIn $BuildFolder -PackagedModulePath $PackagedModulePath
+          }
+        } catch {
+          $ErrorActionPreference = 'Continue'
+          Throw "Unable to puppetize and publish $($Module.Name) for $($VersionAndBuild.Version)"
+          Start-Sleep -Seconds 15
         }
-        $PuppetizeParameters = @{
-          PuppetModuleAuthor      = 'dsc'
-          PowerShellModuleName    = [string]$Module.PowerShellModuleInfo.Name
-          PowerShellModuleVersion = [string]$VersionAndBuild.Version -replace '-', '.'
-        }
-        Write-Host "Puppetizing with: $($PuppetizeParameters | Out-String)"
-        New-PuppetDscModule @PuppetizeParameters
-        $VersionAndBuild.Build += 1
-        $PublishCommand = @(
-          'pdk'
-          'release'
-          "--forge-token=$ENV:FORGE_TOKEN"
-          "--version=$($VersionAndBuild | ConvertFrom-VersionBuild)"
-          '--skip-changelog'
-          '--skip-validation'
-          '--skip-documentation'
-          '--skip-dependency'
-          '--force'
-        ) -Join ' '
-        Write-Host "Executing: $PublishCommand"
-        Invoke-PdkCommand -Path $OutputFolder -Command $PublishCommand -SuccessFilterScript { $_ -match 'Publish to Forge was successful' }
-        Write-Host "Published $($Module.Name) at $($VersionAndBuild | ConvertFrom-VersionBuild)"
       }
     }
   }
